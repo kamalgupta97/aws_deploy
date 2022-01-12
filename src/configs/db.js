3b45e09055c3a413a1517006b165c14e68269c6a
kamal <kamaldro97@gmail.com>
2022-01-13 00:09:11 +0530

    second

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://kamal:kamal@cluster0.kvzdq.mongodb.net/test"
+    "mongodb+srv://aws.000uh.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509"
   );
 };
